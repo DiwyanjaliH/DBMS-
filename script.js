8ecdd00ebcbc31bccab2e7d3ed66ad7068cd1eed
Hiroshima Diwyanjali <168516461+DiwyanjaliH@users.noreply.github.com>
2026-05-27 13:31:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const submitBtn = document.getElementById("submitBtn");
 const notification = document.getElementById("notification");
 const timerDisplay = document.getElementById("timer");
 
-const dropboxLink = "https://www.dropbox.com/request/65div3vmtlfq8si980sk";
+const dropboxLink = "https://www.dropbox.com/request/76mvjgdzn6m5av0phivb";
 
 // ⏳ 60 minutes in ms
 const DURATION = 45 * 60 * 1000;
